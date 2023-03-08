7969b4f408d50932042482946283efab9f29eec6
Andrew <andrew@aguest.me>
2023-03-08 06:03:10 -0600

    day 1 commit

---
+++ 01 - variables.swift
@@ -0,0 +1,14 @@
+/*
+let - defines a constant value
+var - defines a regular value
+*/
+
+let meaning_of_life = 42  // this is ALWAYS the meaning of life
+var current_age = 29  // value that can be changed later
+
+
+print("The meaning of life is \(meaning_of_life)")
+print("You are \(current_age) years old right now")
+
+current_age += 1
+print("Now you are \(current_age) years old")
---
+++ compile instructions.md
@@ -0,0 +1,2 @@
+To compile a .swift file run:
+`swiftc -o <output file name> <input file>.swift`
