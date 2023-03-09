51e226b412cba9393dbaaa08fab8043901381869
Andrew <andrew@aguest.me>
2023-03-08 20:13:42 -0600

    Create type_annotation.swift

---
+++ basics/type_annotation.swift
@@ -0,0 +1,11 @@
+var non_annotated_string = "cool string"
+print(non_annotated_string)
+
+// Define variable with type annotation, but no value at first
+var city: String
+city = "Kansas City"
+print(city)
+
+// Define variable with type annotation and value at the same time
+var state: String = "Missouri"
+print(state)
