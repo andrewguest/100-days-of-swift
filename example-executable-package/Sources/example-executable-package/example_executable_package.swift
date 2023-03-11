34a82ec9a9d67c17bcb91eb615595e0c7799d4f5
andrewguest <andrew@aguest.me>
2023-03-11 16:05:34 -0600

    day 4 - more changes

--- example-executable-package/Sources/example-executable-package/example_executable_package.swift
+++ example-executable-package/Sources/example-executable-package/example_executable_package.swift
@@ -1,8 +1,21 @@
+import Alamofire
+
+
 @main
 public struct example_executable_package {
     public private(set) var text = "Hello, World!"
 
     public static func main() {
         print(example_executable_package().text)
+        get_json_api()
+    }
+
+    func get_json_api() {
+        let url: String = "https://jsonplaceholder.typicode.com/posts/1"
+        let request = AF.request(url)
+
+        request.responseJSON { (data) in
+            print(data)
+        }
     }
 }
