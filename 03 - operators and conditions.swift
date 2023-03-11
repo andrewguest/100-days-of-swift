c61bd8d38f56be3e27c2ffc20f5cfe1a001c0d40
Andrew <andrew@aguest.me>
2023-03-10 19:51:06 -0600

    day 3

---
+++ 03 - operators and conditions.swift
@@ -0,0 +1,46 @@
+// Arithmetic
+
+var firstScore: Int = 10
+var secondScore: Int = 41
+
+var total: Int = firstScore + secondScore
+var difference: Int = firstScore - secondScore
+var product: Int = firstScore * secondScore
+var divided = firstScore / secondScore
+
+print(total)
+print(difference)
+print(product)
+print(divided)
+
+
+// operator overloading
+var firstName = "James"
+var lastName = "Bond"
+var fullName = firstName + " " + lastName
+print(fullName)
+
+
+// compound operators
+var score = 98
+score += 3  // compound operator of "+" and "="
+score -= 1
+print(score)
+
+
+// if loop
+var firstCard = 25
+var secondCard = 82
+
+if firstCard + secondCard == 100 {
+    print("The two values equal 100!")
+} else if firstCard > secondCard {
+    print("firstCard is greater than secondCard")
+} else if firstCard < secondCard {
+    print("firstCard is less than secondCard")
+} else {
+    print("Something else happened")
+}
+
+
+// combining operators
